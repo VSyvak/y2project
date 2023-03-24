7bdc60162955a809b099a7d3a167931469bceb94
Viktor Syvak <X00174084@mytudublin.ie>
2023-03-24 18:46:19 +0000

    first commit

---
+++ public/src/dataAccess/supabase.js
@@ -0,0 +1,21 @@
+/*
+  Supabase configuration.
+*/
+
+// Supabase dependency is imported near end of index.html
+const { createClient } = supabase;
+
+// Get these values from the API section of your Supabase account
+const supabaseUrl = 'https://pylqjswoerizijbqtnci.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5bHFqc3dvZXJpemlqYnF0bmNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzk0MjQxNTEsImV4cCI6MTk5NTAwMDE1MX0.78mDgMpj0YvDG0AxFYqRf8mVDTFroLN-mOnAXYdW0Fg';
+
+// Note text case: Supabase != supabase
+const Supabase = createClient(supabaseUrl, supabaseKey);
+
+console.log('Supabase Instance: ', Supabase);
+
+// Export to allow import elsewhere
+export  {
+  Supabase
+};
+
